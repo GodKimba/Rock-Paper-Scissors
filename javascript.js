e511c44e564ebce1c85420b0453acd52ea983fd8
GodKimba <rafaelfadel2@gmail.com>
2022-04-20 12:51:05 -0300

    finished the rock, paper and scissors game

---
+++ javascript.js
@@ -0,0 +1,47 @@
+let rock = 0;
+let paper = 1;
+let scissors = 2;
+let computerChoice;
+
+// Randomize computer choice
+function computerPlay() {
+  computerChoice = Math.floor(Math.random() * 3);
+  if (computerChoice == rock) {
+    computerChoice = rock;
+  } else if (computerChoice == paper) {
+    computerChoice = paper;
+  } else if (computerChoice == scissors) {
+    computerChoice = scissors;
+  }
+}
+
+// Rock, Paper and Scissors game function
+function playRound(playerSelection, computerChoice) {
+  if (playerSelection == computerChoice) {
+    console.log("Tie");
+  } if (playerSelection == 1 && computerChoice == 2) {
+    console.log("You Lose! Scissors beats Paper");
+  } if (playerSelection == 1 && computerChoice == 0) {
+    console.log("You Win! Paper beats Rock");
+  } if (playerSelection == 2 && computerChoice == 1) {
+    console.log("You Win! Scissors beats Paper");
+  } if (playerSelection == 2 && computerChoice == 0) {
+    console.log("You Lose! Rock beats Scissors");
+  } if (playerSelection == 0 && computerChoice == 1) {
+    console.log("You Lose! Rock beats Paper");
+  } if (playerSelection == 0 && computerChoice == 2) {
+    console.log("You Win! Rock beats Scissors");
+  }
+}
+computerPlay();
+let playerSelection = prompt("Enter your choice: ");
+if (playerSelection == "Rock") {
+    playerSelection = 0
+} else if (playerSelection == "Paper") {
+    playerSelection = 1
+} else if (playerSelection == "Scissors") {
+    playerSelection = 2
+}
+
+
+playRound(playerSelection, computerChoice);
